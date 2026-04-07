2b8341b309b538172c35dbccdb7aecac24edebb4
Jake <105372869@student.swin.edu.au>
2026-04-07 15:36:59 +1000

    Pin all dependencies to fix numpy conflict

--- SolarLogger.py
+++ SolarLogger.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from st_gsheets_connection import GSheetsConnection
+from streamlit_gsheets import GSheetsConnection
 from PIL import Image
 import pandas as pd
 import datetime
